e23fb1186143207a24dca72af9f9d494ce545165
Kevin Jiang <kevin51jiang@email.com>
2019-12-02 01:37:11 -0500

    converted to ccvi colors

--- js/main.js
+++ js/main.js
@@ -133,12 +133,5 @@ jQuery(document).ready(function( $ ) {
     }
   });
 
-  // Buy tickets select the ticket type on click
-  $('#buy-ticket-modal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget);
-    var ticketType = button.data('ticket-type');
-    var modal = $(this);
-    modal.find('#ticket-type').val(ticketType);
-  })
 
 });
